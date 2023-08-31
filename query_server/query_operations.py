4806f5b0e22e87d9b7a071ee7462f2e46ebe9190
fnguyen <OrdinaryNeumann@gmail.com>
2023-08-31 16:35:50 -0400

    Add summary statistics

--- query_server/query_operations.py
+++ query_server/query_operations.py
@@ -1,56 +1,154 @@
 from flask import request, Flask, session
 import requests
 import secrets
+import urllib
 import config
+
+PAGE_SIZE = 10000000
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_bytes(32)
 
+def get_service_info():
+    return {
+        "id": "org.candig.query",
+        "name": "CanDIG query service",
+        "type": {
+            "group": "org.candig",
+            "artifact": "query",
+            "version": "v0.1.0"
+        },
+        "description": "A query microservice for operating with HTSGet & Katsu",
+        "organization": {
+            "name": "CanDIG",
+            "url": "https://www.distributedgenomics.ca"
+        },
+        "version": "0.1.0"
+    }
+
 # Grab a list of donors matching a given filter from the given URL
-def get_donors_from_katsu(url, parameter_list):
+def get_donors_from_katsu(url, param_name, parameter_list):
     permissible_donors = set()
     for parameter in parameter_list:
         # TODO: Fix the page_size call here -- use a consume_all() query like in the frontend
-        treatments = requests.get((url % parameter) + "&page_size=10000000", headers=request.headers)
+        parameters = {
+            param_name: parameter,
+            'page_size': PAGE_SIZE
+        }
+        treatments = requests.get(f"{url}?{urllib.parse.urlencode(parameters)}", headers=request.headers)
         results = treatments.json()['results']
         permissible_donors |= set([result['submitter_donor_id'] for result in results])
     return permissible_donors
 
+def add_or_increment(dict, key):
+    if key in dict:
+        dict[key] += 1
+    else:
+        dict[key] = 1
+
+def get_summary_stats(donors, headers):
+    # Perform (and cache) summary statistics
+    diagnoses = requests.get(f"{config.KATSU_URL}/v2/authorized/primary_diagnoses/?page_size={PAGE_SIZE}",
+        headers=headers).json()['results']
+    # This search is inefficient O(m*n)
+    # Should find a better way (Preferably SQL again)
+    donor_ids = [donor['submitter_donor_id'] for donor in donors]
+    donor_date_of_births = [donor['date_of_birth'] for donor in donors]
+    age_at_diagnosis = {}
+    for diagnosis in diagnoses:
+        if diagnosis['submitter_donor_id'] in donor_ids:
+            donor_idx = donor_ids.index(diagnosis['submitter_donor_id'])
+            diag_date = diagnosis['date_of_diagnosis'].split('-')
+            birth_date = donor_date_of_births[donor_idx].split('-')
+
+            age = int(diag_date[0]) - int(birth_date[0])
+            if int(diag_date[1]) >= int(birth_date[1]):
+                age += 1
+            age = age // 10 * 10
+            if age < 20:
+                add_or_increment(age_at_diagnosis, '0-19 Years')
+            elif age > 79:
+                add_or_increment(age_at_diagnosis, '80+ Years')
+            else:
+                add_or_increment(age_at_diagnosis, f'{age}-{age+9} Years')
+
+    # Treatment types
+    # http://candig.docker.internal:8008/v2/authorized/treatments/
+    treatments = requests.get(f"{config.KATSU_URL}/v2/authorized/treatments/?page_size={PAGE_SIZE}",
+        headers=headers).json()['results']
+    treatment_type_count = {}
+    for treatment in treatments:
+        # This search is inefficient O(m*n)
+        if treatment['submitter_donor_id'] in donor_ids:
+            for treatment_type in treatment['treatment_type']:
+                add_or_increment(treatment_type_count, treatment_type)
+
+    # Cancer types
+    cancer_type_count = {}
+    patients_per_cohort = {}
+    for donor in donors:
+        for cancer_type in donor['primary_site']:
+            if cancer_type in cancer_type_count:
+                cancer_type_count[cancer_type] += 1
+            else:
+                cancer_type_count[cancer_type] = 1
+        program_id = donor['program_id']
+        if program_id in patients_per_cohort:
+            patients_per_cohort[program_id] += 1
+        else:
+            patients_per_cohort[program_id] = 1
+    
+    return {
+        'age_at_diagnosis': age_at_diagnosis,
+        'treatment_type_count': treatment_type_count,
+        'cancer_type_count': cancer_type_count,
+        'patients_per_cohort': patients_per_cohort
+    }
+
 @app.route('/query')
 def query(treatment="", primary_site="", chemotherapy="", immunotherapy="", hormone_therapy="", chrom="", gene="", page=0, page_size=10, session_id=""):
     # NB: We're still doing table joins here, which is probably not where we want to do them
-    # We're grabbing (and storing in memory) all the data in Katsu with the below request
+    # We're grabbing (and storing in memory) all the donor data in Katsu with the below request
 
     # Query the appropriate Katsu endpoint
-    url = f"{config.KATSU_URL}/v2/authorized/donors/?page_size=10000000"
+    params = { 'page_size': PAGE_SIZE }
+    url = f"{config.KATSU_URL}/v2/authorized/donors/"
     if primary_site != "":
-        url += "&primary_site=" + ",".join(primary_site)
-    r = requests.get(url,
+        params['primary_site'] = ",".join(primary_site)
+    r = requests.get(f"{url}?{urllib.parse.urlencode(params)}",
         # Reuse their bearer token
         headers=request.headers)
     donors = r.json()['results']
-    print(donors)
 
     # Will need to look into how to go about this -- ideally we implement this into the SQL in Katsu's side
-    # NB: Double check if using URLs like below are weak to an injection attack when I'm not tired
     filters = [
-        (treatment, f"{config.KATSU_URL}/v2/authorized/treatments/?treatment_type=%s"),
-        (chemotherapy, f"{config.KATSU_URL}/v2/authorized/chemotherapies/?drug_name=%s"),
-        (immunotherapy, f"{config.KATSU_URL}/v2/authorized/immunotherapies/?drug_name=%s"),
-        (hormone_therapy, f"{config.KATSU_URL}/v2/authorized/hormone_therapies/?drug_name=%s")
+        (treatment, f"{config.KATSU_URL}/v2/authorized/treatments/", 'treatment_type'),
+        (chemotherapy, f"{config.KATSU_URL}/v2/authorized/chemotherapies/", 'drug_name'),
+        (immunotherapy, f"{config.KATSU_URL}/v2/authorized/immunotherapies/", 'drug_name'),
+        (hormone_therapy, f"{config.KATSU_URL}/v2/authorized/hormone_therapies/", 'drug_name')
     ]
-    for (this_filter, url) in filters:
+    for (this_filter, url, param_name) in filters:
         if this_filter != "":
-            print(this_filter)
             permissible_donors = get_donors_from_katsu(
                 url,
+                param_name,
                 this_filter
             )
             donors = [donor for donor in donors if donor['submitter_donor_id'] in permissible_donors]
-            print(donors)
+    summary_stats = get_summary_stats(donors, request.headers)
     
-    # TODO: Cache the above list of donor IDs, then return donor_with_clinical_data per-page as a response
+    # TODO: Cache the above list of donor IDs and summary statistics
+    ret_donors = [donor['submitter_donor_id'] for donor in donors[(page*page_size):((page+1)*page_size)]]
+    params = {
+        'page_size': PAGE_SIZE,
+        'donors': ','.join(ret_donors)
+    }
+    r = requests.get(f"{config.KATSU_URL}/v2/authorized/donor_with_clinical_data/?{urllib.parse.urlencode(params)}",
+        headers=request.headers)
+    full_data = r.json()
+    full_data['summary'] = summary_stats
+
     # Add prev and next parameters to the repsonse, appending a session ID.
     # Essentially we want to go session ID -> list of donors
     # and then paginate the list of donors, calling donors_with_clinical_data on each before returning
-    return str(donors), 200
+    return str(full_data), 200
--- query_server/server.py
+++ query_server/server.py
@@ -6,43 +6,11 @@ import logging
 from config import PORT, DEBUG_MODE
 
 # Create the application instance
-app = connexion.App(__name__, specification_dir='./')
+app = connexion.FlaskApp(__name__, specification_dir='./', options={"swagger_url": "/api"})
 app.app.config['SECRET_KEY'] = secrets.token_bytes(32)
 CORS(app.app)
 
 app.add_api('openapi.yaml', pythonic_params=True, strict_validation=True)
-
-# Just leaving this here as a note: these are all of the pythonic params that
-# will get shadowed by pythonic_params:
-# ['__name__', '__doc__', '__package__', '__loader__', '__spec__', '__build_class__', '__import__', 'abs', 'all', 'any', 'ascii', 'bin', 'breakpoint', 'callable', 'chr', 'compile', 'delattr', 'dir', 'divmod', 'eval', 'exec', 'format', 'getattr', 'globals', 'hasattr', 'hash', 'hex', 'id', 'input', 'isinstance', 'issubclass', 'iter', 'len', 'locals', 'max', 'min', 'next', 'oct', 'ord', 'pow', 'print', 'repr', 'round', 'setattr', 'sorted', 'sum', 'vars', 'None', 'Ellipsis', 'NotImplemented', 'False', 'True', 'bool', 'memoryview', 'bytearray', 'bytes', 'classmethod', 'complex', 'dict', 'enumerate', 'filter', 'float', 'frozenset', 'property', 'int', 'list', 'map', 'object', 'range', 'reversed', 'set', 'slice', 'staticmethod', 'str', 'super', 'tuple', 'type', 'zip', '__debug__', 'BaseException', 'Exception', 'TypeError', 'StopAsyncIteration', 'StopIteration', 'GeneratorExit', 'SystemExit', 'KeyboardInterrupt', 'ImportError', 'ModuleNotFoundError', 'OSError', 'EnvironmentError', 'IOError', 'EOFError', 'RuntimeError', 'RecursionError', 'NotImplementedError', 'NameError', 'UnboundLocalError', 'AttributeError', 'SyntaxError', 'IndentationError', 'TabError', 'LookupError', 'IndexError', 'KeyError', 'ValueError', 'UnicodeError', 'UnicodeEncodeError', 'UnicodeDecodeError', 'UnicodeTranslateError', 'AssertionError', 'ArithmeticError', 'FloatingPointError', 'OverflowError', 'ZeroDivisionError', 'SystemError', 'ReferenceError', 'MemoryError', 'BufferError', 'Warning', 'UserWarning', 'DeprecationWarning', 'PendingDeprecationWarning', 'SyntaxWarning', 'RuntimeWarning', 'FutureWarning', 'ImportWarning', 'UnicodeWarning', 'BytesWarning', 'ResourceWarning', 'ConnectionError', 'BlockingIOError', 'BrokenPipeError', 'ChildProcessError', 'ConnectionAbortedError', 'ConnectionRefusedError', 'ConnectionResetError', 'FileExistsError', 'FileNotFoundError', 'IsADirectoryError', 'NotADirectoryError', 'InterruptedError', 'PermissionError', 'ProcessLookupError', 'TimeoutError', 'open', 'quit', 'exit', 'copyright', 'credits', 'license', 'help', '_', 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'] j
-
-
-@app.route('/')
-def index():
-    return {
-        "id": "org.candig.query",
-        "name": "CanDIG query service",
-        "type": {
-            "group": "org.candig",
-            "artifact": "query",
-            "version": "v0.1.0"
-        },
-        "description": "A query microservice for operating with HTSGet & Katsu",
-        "organization": {
-            "name": "CanDIG",
-            "url": "https://www.distributedgenomics.ca"
-        },
-        "version": "0.1.0"
-    }
-
-@app.route('/test')
-def test():
-    if "request" not in session:
-        session["request"] = 1
-    else:
-        session["request"] = session["request"] + 1
-    session.modified = True
-    return str(session["request"]), 200
 
 if __name__ == '__main__':
     logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
